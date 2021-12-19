7d2bf8d05718ebc2f9b2361197bbb6c848e9c99d
yaok <yaok@pingansec.com>
2021-12-19 20:46:38 +0800

    迭代器

--- src/com/designmode/m_iterator/v4/Main.java
+++ src/com/designmode/m_iterator/v4/Main.java
@@ -13,7 +13,11 @@ public class Main {
         for (int i=0;i<15;i++){
             list.add(new String("s" + i));
         }
-
         System.out.println(list.size());
+
+        ArrayList_ al = (ArrayList_) list;
+        for (int i = 0;i<list.size();i++){
+            // 如果使用这种遍历方式，就不能实现通用了
+        }
     }
 }
