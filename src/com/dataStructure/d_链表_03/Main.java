df5f15cf2df54b8ab46c4438fdf97b6addc28fca
姚坤 <yaok@pingansec.com>
2022-03-02 15:55:20 +0800

    双向循环链表

--- src/com/dataStructure/d_链表_03/Main.java
+++ src/com/dataStructure/d_链表_03/Main.java
@@ -55,6 +55,7 @@ public class Main {
         System.out.println(list3);
 
 
+        //// 测试单向循环链表
         System.out.println("===========单向循环链表============");
         LinkList_SingleCircle<Object> list4 = new LinkList_SingleCircle<>();
 
@@ -70,5 +71,51 @@ public class Main {
         System.out.println(list4);
         list4.remove(list4.size()-1);
         System.out.println(list4);
+
+
+        //// 测试双向循环链表
+        System.out.println("===========双向循环链表============");
+        LinkList_DoubleCircle<Object> list5 = new LinkList_DoubleCircle<Object>();
+        list5.add(11);
+        list5.add(22);
+        list5.add(33);
+        list5.add(44);
+        list5.add(0,55);
+        list5.add(2,66);
+
+
+        System.out.println(list5);
+        list5.remove(list5.size()-1);
+        System.out.println(list5);
+
+
+        //// 双向循环链表的应用
+        System.out.println("===========双向循环链表解决约瑟夫问题===========");
+        LinkList_DoubleCircle<Object> list6 = new LinkList_DoubleCircle<Object>();
+        list6.add(1);
+        list6.add(2);
+        list6.add(3);
+        list6.add(4);
+        list6.add(5);
+        list6.add(6);
+        list6.add(7);
+        list6.add(8);
+        int i = 0;
+        int j = 1;
+        while (!list6.isEmpaty()) {
+            //System.out.println(i + " " + j);
+            if((j) % 3 == 0) {
+                System.out.println(list6.remove(i));
+                //System.out.println(delIndex);
+                //i = 0;
+            }else {
+                i++;
+            }
+
+            if (i == list6.size()) {
+                i = 0;
+            }
+            j++;
+        }
     }
 }
