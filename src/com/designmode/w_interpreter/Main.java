2b17421f595f382f2c4c44df3d29b33ee9d1afc9
yaok <yaok@pingansec.com>
2022-03-28 10:34:33 +0800

    解释器模式-给定一个语言，定义它的语法表示，并定义一个解释器，这个解释器使用该标识来解释语言中的句子

---
+++ src/com/designmode/w_interpreter/Main.java
@@ -0,0 +1,12 @@
+package com.designmode.w_interpreter;
+
+/**
+ * 解释器模式-给定一个语言，定义它的语法表示，并定义一个解释器，这个解释器使用该标识来解释语言中的句子
+ *
+ * 很少使用
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
