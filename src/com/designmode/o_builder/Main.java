824782335dceff4ad74f23e1291f6c79144fc269
姚坤 <yaok@pingansec.com>
2023-02-20 14:19:48 +0800

    build

--- src/com/designmode/o_builder/Main.java
+++ src/com/designmode/o_builder/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         TerrainBuilder builder = new ComplexTerrainBuilder();
         Terrain t = builder.buildFort().buildMine()
-                .buildMine().build();
+                .buildWall().build();
 
         Person p = new Person.PersonBuilder()
                 .basicInfo(1,"zhangsan",18)
